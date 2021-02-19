c9ae7f777ef4d2931179fe8c358328e62aa1874a
MaRTiN <53973885+imartin-ff@users.noreply.github.com>
2021-02-19 22:21:42 +0200

    v0.3

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  // document.getElementsByClassName("skills__bar");
+  var sBars = document.querySelectorAll(".skills__bar");
+  sBars.forEach(function(item){
+    if(item.hasAttribute("data-percent")){
+      item.style.width = item.dataset.percent + "%";
+    }
+  })
+
+
+
+
+
+})
